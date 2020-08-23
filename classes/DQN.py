0176496330025507905ba4e50fbae805a4530c73
Danielle.Potts+1-at-253621763092 <danielle.potts@accenture.com>
2020-08-23 18:47:08 +0100

    change linear to output 5 actions

--- classes/DQN.py
+++ classes/DQN.py
@@ -9,14 +9,25 @@ class DQN(nn.Module):
         if(model.upper() == "LINEAR"):
              self.fc1, self.fc2, self.out = self.linear_network(img_height, img_width)
         elif(model.upper() == "CNN"):
-            print("CNN")
+            return self.convolutional_network(img_height, img_width)
        
     def linear_network(self, img_height, img_width):
         fc1 = nn.Linear(in_features=img_height*img_width*3, out_features=24)
         fc2 = nn.Linear(in_features=24,  out_features=32)
-        out = nn.Linear(in_features=32, out_features=2)
+        out = nn.Linear(in_features=32, out_features=5)
 
         return fc1, fc2, out
+    def convolutional_network(self, img_height, img_width, rgb):
+        model = nn.Sequential(
+            nn.Conv2d(tau, 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        return model
     def forward(self, t):
         t = t.flatten(start_dim=1)
         t = F.relu(self.fc1(t)) 
